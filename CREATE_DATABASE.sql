db9e555042bb68ece45c5c9a8041483adbaab47e
mateonaval23 <matias.lopezbarrios@gmail.com>
2020-06-21 21:26:39 -0300

    create project structure

---
+++ CREATE_DATABASE.sql
@@ -0,0 +1,55 @@
+-- Creo la base de datos si todavia no se creo.
+CREATE DATABASE IF NOT EXISTS delilahresto;
+
+-- Usuo la base de datos creada para crear las tablas necesarias
+USE delilahresto;
+
+-- Creo tablas de la base de datos
+CREATE TABLE usuarios(
+	id int not null auto_increment,
+    nombres varchar(150),
+    correo varchar(150) not null,
+    username varchar(150) not null,
+	password varchar(16)  not null,
+    direccion varchar(150),
+    telefono varchar(10),
+    tipousuario varchar(1),
+    primary key (id)
+);
+
+CREATE TABLE mediosdepago(
+	id int not null auto_increment,
+    descripcion varchar(150) not null,
+    activo varchar(1),
+    primary key (id)
+);
+
+CREATE TABLE pedidos(
+	id int not null auto_increment,
+    numero int not null,
+    hora datetime not null,
+    estado varchar(1),
+    usuarioId int not null,
+    mediodepagoId int not null,
+    primary key (id),
+    foreign key (usuarioId) references usuarios(id),
+    foreign key (mediodepagoId) references mediosdepago(id)
+);
+
+
+CREATE TABLE platos(
+	id int not null auto_increment,
+    nombre varchar(150) not null,
+    urlImagen varchar(200),
+    precio dec(5,2),
+	primary key (id)
+);
+
+CREATE TABLE pedidosdetalle(
+	id int not null auto_increment,
+    pedidosId int not null,
+    platosId int not null,
+    primary key (id),
+    foreign key (pedidosId) references pedidos(id),
+    foreign key (platosId) references platos(id)
+);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # delilahResto
 Proyecto delilah resto para Acamica
+
+1. Como primer paso se debera crear la base de datos. Para eso deberan correr el script de creacion de bas de datos ubicado en el archivo **CREATE_DATABASE.sql**.
+2. Una vez creada la base de datos debera modificarse los datos de conexión ubicados en el archivo del proyecto `data/mysql.js`.
+```js
+const sequelize = new Sequelize('mysql://root@localhost:3306/delilahresto');
+```
+3. Desde una consola de comandos, posicionarse en el proyecto, y ejecutar el siguiente comando: `npm i`. Este comando instalara todos las dependencias necesarias para que se pueda correr el proyecto en su maquina.
+4. Cuando haya finalizado toda la instalación, deberan ejecutar el comando `npm run start`. Este mismo se encuentra configurado como script en el archivo `package.json`.
+5. Para consultar los endpoint de la Web API, pueden consultar el archivo spec.yml referido en la especificacion de Open API (Swagger).
+
+
+
